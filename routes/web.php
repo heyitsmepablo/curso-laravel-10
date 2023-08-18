449364ec285a8a390f4c5611de7034792acf9e76
Pablo <pabloed0009@gmail.com>
2023-08-18 17:32:53 -0300

    Update: Aula 10

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\Admin\{SupportController};
+use App\Http\Controllers\Site\SiteController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +15,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::get('contact', [SiteController::class, 'contact']);
+
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/supports', [SupportController::class, 'index' ])->name('supports.index');
